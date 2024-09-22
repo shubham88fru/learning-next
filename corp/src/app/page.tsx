980f0c8f2ca130fe1a23ae92ca00c6b4626d0162
shubham88fru <shubham88fru@yahoo.com>
2024-09-21 17:13:27 -0700

    file based routing and layout

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,5 +1,9 @@
+
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div>
+      
+      Home Page
+    </div>
   );
 }
---
+++ corp/src/app/reliability/page.tsx
@@ -0,0 +1,3 @@
+export default function Reliabilty() {
+  return <div>Reliability page</div>
+}
